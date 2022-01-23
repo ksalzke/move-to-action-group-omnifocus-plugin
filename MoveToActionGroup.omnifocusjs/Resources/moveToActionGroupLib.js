0e0bc2817660bf160a45f2491fde7781e8942000
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-01-23 15:55:08 +1100

    style: :bulb: tidy up comments etc
    -

--- MoveToActionGroup.omnifocusjs/Resources/moveToActionGroupLib.js
+++ MoveToActionGroup.omnifocusjs/Resources/moveToActionGroupLib.js
@@ -3,9 +3,7 @@
   const lib = new PlugIn.Library(new Version('1.0'))
 
   lib.goTo = async (task) => {
-    'in lib goTo function'
-    // new tab - only Mac supported
-    if (Device.current.mac) await document.newTabOnWindow(document.windows[0])
+    if (Device.current.mac) await document.newTabOnWindow(document.windows[0]) // new tab - only Mac supported
     URL.fromString('omnifocus:///task/' + task.containingProject.id.primaryKey).call(() => {})
     URL.fromString('omnifocus:///task/' + task.id.primaryKey).call(() => {})
   }
