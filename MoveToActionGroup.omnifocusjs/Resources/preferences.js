bfedb0fde55a84fd6ac1d3b04e633099e29d25e7
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-06-05 20:32:53 +1000

    feat: :sparkles: Add preference to auto-include top-level or all action groups

--- MoveToActionGroup.omnifocusjs/Resources/preferences.js
+++ MoveToActionGroup.omnifocusjs/Resources/preferences.js
@@ -6,15 +6,18 @@
 
     // get current preferences or set defaults if they don't yet exist
     const actionGroupTag = lib.prefTag('actionGroupTag')
+    const autoInclude = lib.autoInclude()
 
     // create and show form
     const form = new Form()
     const tagNames = flattenedTags.map(t => t.name)
     form.addField(new Form.Field.Option('actionGroupTag', 'Action Group Tag', flattenedTags, tagNames, actionGroupTag, null))
+    form.addField(new Form.Field.Option('autoInclude', 'Automatically Include Action Groups', ['none', 'top', 'all'], ['None', 'Top-Level', 'All'], autoInclude))
     await form.show('Preferences: Move To Action Group', 'OK')
 
     // save preferences
     syncedPrefs.write('actionGroupTagID', form.values.actionGroupTag.id.primaryKey)
+    syncedPrefs.write('autoInclude', form.values.autoInclude)
   })
 
   action.validate = function (selection, sender) {
