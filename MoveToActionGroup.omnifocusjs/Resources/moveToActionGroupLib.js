5f91ee9f5ee52c2c65dcc6c4913705cc7a331391
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2022-06-05 14:10:48 +1000

    feat: :sparkles: add preference for designating the tag for action groups

--- MoveToActionGroup.omnifocusjs/Resources/moveToActionGroupLib.js
+++ MoveToActionGroup.omnifocusjs/Resources/moveToActionGroupLib.js
@@ -1,4 +1,4 @@
-/* global PlugIn Version Device */
+/* global PlugIn Version Device Alert Tag */
 (() => {
   const lib = new PlugIn.Library(new Version('1.0'))
 
@@ -6,6 +6,38 @@
     if (Device.current.mac) await document.newTabOnWindow(document.windows[0]) // new tab - only Mac supported
     URL.fromString('omnifocus:///task/' + task.containingProject.id.primaryKey).call(() => {})
     URL.fromString('omnifocus:///task/' + task.id.primaryKey).call(() => {})
+  }
+
+  lib.loadSyncedPrefs = () => {
+    const syncedPrefsPlugin = PlugIn.find('com.KaitlinSalzke.SyncedPrefLibrary')
+
+    if (syncedPrefsPlugin !== null) {
+      const SyncedPref = syncedPrefsPlugin.library('syncedPrefLibrary').SyncedPref
+      return new SyncedPref('com.KaitlinSalzke.MoveToActionGroup')
+    } else {
+      const alert = new Alert(
+        'Synced Preferences Library Required',
+        'For the Move To Action Group plug-in to work correctly, the \'Synced Preferences for OmniFocus\' plug-in (https://github.com/ksalzke/synced-preferences-for-omnifocus) is also required and needs to be added to the plug-in folder separately. Either you do not currently have this plugin installed, or it is not installed correctly.'
+      )
+      alert.show()
+    }
+  }
+
+  lib.prefTag = prefTag => {
+    const preferences = lib.loadSyncedPrefs()
+    const tagID = preferences.readString(`${prefTag}ID`)
+
+    if (tagID !== null && Tag.byIdentifier(tagID) !== null) return Tag.byIdentifier(tagID)
+    return null
+  }
+
+  lib.getPrefTag = async (prefTag) => {
+    const tag = lib.prefTag(prefTag)
+
+    if (tag !== null) return tag
+    // if not set, show preferences pane and then try again)
+    await this.action('preferences').perform()
+    return lib.getPrefTag(prefTag)
   }
 
   return lib
