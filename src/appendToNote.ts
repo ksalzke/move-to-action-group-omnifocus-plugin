faebc24d95c48a6487faba85e9da4a4a68382c00
Kaitlin Salzke <kaitlinsalzke@gmail.com>
2023-09-04 20:52:47 +1000

    feat: :sparkles: add 'append to note' action

---
+++ src/appendToNote.ts
@@ -0,0 +1,33 @@
+(() => {
+  const action = new PlugIn.Action(async (selection: Selection) => {
+
+    const lib = this.libraries[0]
+    const tasks = [...selection.tasks, ...selection.projects.map(project => project.task)]
+
+    const groups = await lib.potentialActionGroups(null)
+
+    const fuzzySearchLib = lib.getFuzzySearchLib()
+    const searchForm = fuzzySearchLib.searchForm(groups, groups.map(g => g.name), null, null)
+
+    await searchForm.show('Select Task', 'Append To Note')
+    const location = searchForm.values.menuItem
+
+    // append to note
+    for (const task of tasks) {
+      location.note = location.note + '\n- ' + task.name
+      deleteObject(task)
+    }
+
+  })
+
+  action.validate = (selection: Selection) => {
+
+    // valid if tasks are selected and they all belong to the same project/assigned project
+    const tasks = [...selection.tasks, ...selection.projects.map(project => project.task)]
+
+    if (tasks.length == 0) return false
+    else return true
+  }
+
+  return action
+})()
